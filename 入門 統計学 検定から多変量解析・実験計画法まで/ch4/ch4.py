87c94d490f61dc10688c01aaa332d6c61302fb5b
djakartatrap <djakartatrap@gmail.com>
2018-03-16 19:57:44 +0900

    今日のお勉強分をコミット

--- 入門 統計学 検定から多変量解析・実験計画法まで/ch4/ch4.py
+++ 入門 統計学 検定から多変量解析・実験計画法まで/ch4/ch4.py
@@ -15,6 +15,8 @@ upper = mean_sample + 1.96*(math.sqrt(var_population / n_sample))
 # 95%信頼限界の下限
 lower = mean_sample - 1.96*(math.sqrt(var_population / n_sample))
 
+print(upper, lower)
+
 # 例題
 # 中国産キャベツの輸入量と，中国産野菜に関する新聞記事の掲載回数との関係について調べたところ（2000年1月～2006年6月），
 # 1回の掲載につき輸入量が月平均で165.0トン減少していたことがわかりました。
@@ -33,4 +35,24 @@ upper = mean_sample + 1.65 * std_sample / math.sqrt(n_sample)
 
 lower = mean_sample - 1.65 * std_sample / math.sqrt(n_sample)
 
-print(lower)
+print(lower, upper)
+
+
+# 例題
+# ある日，ある酪農家が，搾乳中のホルスタイン5頭の乳量を調べたら，
+# 1頭あたりの平均乳量は22.1リットル，標本標準偏差は6.5リットルでした。
+# この農家が飼養しているホルスタインの1頭あたりの乳量（／日）を，
+# 信頼係数95％で推定してください。
+
+sampleSize = 5
+mean_sample = 22.1
+sd_sample = 6.5
+
+# 標本サイズが小さい、母数未知、母平均の推定＝ 統計量 T を使った推定をする。
+# t分布表から、自由度(df)が4の時の、95%信頼係数の数値は、2.776であることが、ｔ分布表から分かる。
+# 信頼限界の下限と上限を出す式も、Tから算出できるので、算出すると・・・
+
+lower = mean_sample - 2.776 * (sd_sample / math.sqrt(sampleSize - 1))
+upper = mean_sample + 2.776 * (sd_sample / math.sqrt(sampleSize - 1))
+
+print(lower, upper)
